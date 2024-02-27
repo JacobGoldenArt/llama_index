fe7d79a3a7fa93644e21c3452c490e50762b4134
Jacob Golden <126922704+JacobGoldenArt@users.noreply.github.com>
2024-02-27 12:43:27 -0800

    Update to Mistral New Endpoints in utils.py
    On 2/26/2024 Mistral Announced changes to their API endpoints. This pull request updates the model dictionary with the currently available endpoints.

--- llama-index-integrations/llms/llama-index-llms-mistralai/llama_index/llms/mistralai/utils.py
+++ llama-index-integrations/llms/llama-index-llms-mistralai/llama_index/llms/mistralai/utils.py
@@ -1,11 +1,21 @@
 from typing import Dict
 
 MISTRALAI_MODELS: Dict[str, int] = {
-    "mistral-tiny": 32000,
-    "mistral-small": 32000,
-    "mistral-medium": 32000,
-    "mistral-large": 32000,
+    "open-mistral-7b": 8000,
+    "open-mixtral-8x7B": 32000, # This endpoint is currently returning a Status 400 But should work according to their documentation.
+    "mistral-small-2312": 32000, # This is an allia for open-mixtral-8x7B
+    "mistral-small-latest": 32000,
+    "mistral-medium-latest": 32000,
+    "mistral-large-latest": 32000,
 }
+
+# Mistral AI Changes 2-26-2024 provides five API endpoints:
+
+# open-mistral-7b (aka mistral-tiny-2312) - The endpoint mistral-tiny will be deprecated in May 2024.
+# open-mixtral-8x7b (aka mistral-small-2312) - (Note: This endpoint is currently returning Status: 400.) The endpoint mistral-small will be deprecated in May 2024.
+# mistral-small-latest (aka mistral-small-2402) - new model
+# mistral-medium-latest (aka mistral-medium-2312) - The previous mistral-medium has been dated and tagged as mistral-medium-2312. The endpoint mistral-medium will be deprecated in May 2024.
+# mistral-large-latest (aka mistral-large-2402) - New Flagship model
 
 
 def mistralai_modelname_to_contextsize(modelname: str) -> int:
